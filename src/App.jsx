0ae05ef574a51f3e0ec90bb2752e856d7b4d2c6d
Douglas Duarte <douglasduarte.eng@gmail.com>
2023-01-17 21:19:15 -0300

    style: Header and Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
-function App() {
-    return <h1>Hello World</h1>;
-}
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Siderbar";
+import { Post } from "./components/Post";
+import styles from "./App.module.css";
+import "./global.css";
 
-export default App;
+export function App() {
+    return (
+        <div>
+            <Header />
+            <div className={styles.wrapper}>
+                <Sidebar />
+                <main>
+                    <Post />
+                </main>
+            </div>
+        </div>
+    );
+}
