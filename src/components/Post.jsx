bacba3be77a720b1ecb09340de782560d7f9894a
Douglas Duarte <douglasduarte.eng@gmail.com>
2023-01-19 06:28:26 -0300

    feat: delete comments in posts

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -33,6 +33,14 @@ export function Post({ author, content, publishedAt }) {
         setNewCommentText(e.target.value);
     }
 
+    function deleteComment(commentToDelete) {
+        const commentsWithoutDeletedOne = comments.filter((comment) => {
+            return comment !== commentToDelete;
+        });
+
+        setComments(commentsWithoutDeletedOne);
+    }
+
     return (
         <article className={styles.post}>
             <header>
@@ -83,7 +91,13 @@ export function Post({ author, content, publishedAt }) {
             <div className={styles.commentList}>
                 {comments.length > 0 &&
                     comments.map((comment) => {
-                        return <Comment content={comment} key={comment} />;
+                        return (
+                            <Comment
+                                onDeleteComment={deleteComment}
+                                content={comment}
+                                key={comment}
+                            />
+                        );
                     })}
             </div>
         </article>
